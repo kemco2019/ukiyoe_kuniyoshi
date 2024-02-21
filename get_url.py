d1247f713da3b7112e06157a07761aaed4364c60
kemco2019 <128669621+kemco2019@users.noreply.github.com>
2024-02-21 11:00:38 +0900

    Update get_url.py

--- get_url.py
+++ get_url.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 # Copyright © 2001-2023 Python Software Foundation; All Rights Reserved
 
-discord_token = "MTEwMTM1OTU2MjA3NTk0NzA5OQ.G2lZ1p.q6WH3HbcMBAPBQ3bpSij2JTrLbG_d7Yp-pFr5k"  # ukiyoe-bot
+discord_token = "DISCORD_TOKEN_OF_YOUR_BOT"
 client = commands.Bot(command_prefix="*", intents=discord.Intents.all())
 
 @client.event
