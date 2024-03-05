bb4416bac09e3f331d874e99cf45773c9fc5a957
kemco2019 <128669621+kemco2019@users.noreply.github.com>
2024-03-05 15:29:35 +0900

    Add files via upload

---
+++ get_pos.py
@@ -0,0 +1,7 @@
+import pyautogui as pag
+
+m_posi_x, m_posi_y = pag.position()
+print('現在のマウスカーソル位置 x: ',m_posi_x)
+print('現在のマウスカーソル位置 y: ',m_posi_y)
+
+#print(pag.locateCenterOnScreen('~/Desktop/kuniyoshi4.jpeg'))
